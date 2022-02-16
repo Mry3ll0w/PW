a580e79b6045ca789ca5165cd72a23e28e2dc1ae
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2022-02-16 09:08:01 +0100

    Initial commit

---
+++ ejemplo.php
@@ -0,0 +1,20 @@
+<html>
+<title>COSAS VISTAS EN CLASE PHP</title>
+<p>
+<?php
+
+echo 'Usar comillas simples es mejor para cadenas literales, si usas las dobles imprime variables con dollar';
+echo "<br>";
+echo 'Con settype se puede definir un tipo fijo a una variable, estas pueden cambiar de tipo (python)';
+$a = 'El pepe';
+echo "<br>";
+echo "$a";
+$a = 2000;
+
+echo "<br>";
+
+echo "Definimos constantes con define([nombre],[valor])"
+
+?>
+</p>
+</html>
