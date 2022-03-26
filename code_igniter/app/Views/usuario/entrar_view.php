61f55826baacb71798c188fcc478917c0c645ff7
Mry3ll0w <aradxc56@gmail.com>
2022-03-26 20:59:00 +0100

    Creadas las vistas entrar view, login view, register view

---
+++ code_igniter/app/Views/usuario/entrar_view.php
@@ -0,0 +1,4 @@
+
+<p><a href="/articulo/create">Crea Un articulo</a></p>
+
+<p><a href="/articulos">Mostrar articulos</a></p>
---
+++ code_igniter/app/Views/usuario/register_view.php
@@ -0,0 +1,21 @@
+<?= \Config\Services::validation()->listErrors(); ?>
+<form action="/usuario/create" method="post">
+     <?= csrf_field() ?>
+    <label for="name">Nombre</label>
+      <input type="input" name="name" /><br />
+    <br></br>
+     <label for="correo">Corrreo</label>
+      <input type="input" name="correo" /><br />
+     <br></br>
+     <label for="user">Usuario</label>
+     <input type="input" name="user" /<br />
+    <br></br>
+    <label for="pass">Contraseña</label>
+     <input type="input" name="pass" /><br />
+     <br></br>
+      <label for="pass2"> Repetir Contraseña</label>
+     <input type="input" name="pass2" /><br />
+     <br></br>
+     <input type="submit" name="submit" value="Create usuario item" />
+    <p><a href="/">Inicio Sesión</a></p>
+</form>
