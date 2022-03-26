8e47187854f75d913ddb5a713b9c0822c9ff8f41
Mry3ll0w <aradxc56@gmail.com>
2022-03-26 12:15:39 +0100

    Areglado overview y se ven los artiuculos

--- code_igniter/app/Views/articulo/overview.php
+++ code_igniter/app/Views/articulo/overview.php
@@ -1,23 +1,16 @@
 
 <p><a href="/articulo/create">Create article</a></p>
 
-<?php if(! empty($articulos) && is_array($articulos)) : ?>
 
-    <?php  foreach($articulos as $articulos): ?>
-        <h3><?php esc($articulos_item['item']); ?></h3>
-
+<?php if (! empty($articulos) && is_array($articulos)) : ?>
+    <?php foreach ($articulos as $articulos_item): ?>
+        <h3><?= esc($articulos_item['title']); ?></h3>
         <div class="main">
-            <?php
-                esc($articulos_item['cuerpo']);
-            ?>
+             <?= esc($articulos_item['cuerpo']); ?>
         </div>
-
-        <p><a href="/articulo/<?php esc($articulos_item['id'],'url'); ?>">View article</a></p>
-
+        <p><a href="/articulo/<?= esc($articulos_item['id'], 'url'); ?>">View article</a></p>
     <?php endforeach; ?>
-    
-<?php else: ?>
-
-    <h3>No articulo</h3>
+<?php else : ?>
+    <h3>No Articulo</h3>
     <p>No hay articulos</p>
 <?php endif ?>
