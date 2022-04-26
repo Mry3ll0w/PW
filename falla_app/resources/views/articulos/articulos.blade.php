4393f0b3987681b3fbc58fefe0b1d857445b16da
MrY3ll0w <aradxc56@gmail.com>
2022-04-26 13:38:47 +0200

    creado dirrectorio base symfony :sparkles:

--- falla_app/resources/views/articulos/articulos.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.header')
-@section('contenido')
-<h2> Bienvenido a la web sobre artículos </h2>
-<a href=" {{route('insertar_articulo')}}" title="Añadir articulo">Añadir articulo</a>
-<p>Estos son los artículos publicados.</p>
-@endsection
